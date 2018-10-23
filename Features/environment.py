fe2b4806924468cf673dd95c57817c1d06a77ca5
Perverseq <luchniknya@gmail.com>
2018-10-24 00:08:08 +0500

    v0.2

--- Features/environment.py
+++ Features/environment.py
@@ -1,5 +1,14 @@
 import behave
 from selenium import webdriver
+import os
 
 def before_all(context):
     context.driver = webdriver.Chrome(".//chromedriver.exe")
+    dirs = os.listdir()
+    create = True
+    for index in range(len(dirs)):
+        if dirs[index] == "Screens":
+            create == False
+            return
+    if create == True:
+        os.makedirs(".\\Screens\\")
--- Features/steps/Sc.py
+++ Features/steps/Sc.py
@@ -1,6 +1,8 @@
 from behave import *
 import time
 from datetime import datetime
+from selenium.common.exceptions import NoSuchElementException
+from PIL import ImageGrab
 
 
 @when("Получить имя канала из файла channel.txt")
@@ -9,9 +11,15 @@ def step_impl(context):
         context.channel_name = channel_name_f.readline()
 
 
-@step("Зайти на этот канал на ютубе")
+@step("Зайти на этот канал на ютубе, если канала нет, сделать скриншот")
 def step_impl(context):
     context.driver.get("http:\\youtube.com\\" + context.channel_name)
+    if context.driver.title == "404 Not Found":
+        screen = ImageGrab.grab()
+        screen.save(".\\Screens\\invalid_channel.jpg")
+        skip_sc(context)
+    else:
+        pass
 
 
 @step("Развернуть браузер на весь экран")
@@ -25,9 +33,12 @@ def step_impl(context):
 @then('Спарсить количество подписчиков в файл')
 def step_impl(context):
     element = context.driver.find_element_by_xpath('//*[@id="subscriber-count"]')
+    name_channel = str(context.driver.find_element_by_id("channel-title").text)
     subs_yet = str(element.text)
     with open(".//Subs.txt", "a") as subs_count:
-        subs_count.write("На канале {0} {1} {2}.\n".format(context.channel_name, subs_yet, datetime.now()))
+        subs_count.write("На канале {0} {1} {2}.\n".format(name_channel, subs_yet, datetime.now()))
+    screen = ImageGrab.grab()
+    screen.save(".\\Screens\\{}.jpg".format(subs_yet))
 
 
 @step("Обновить страницу")
@@ -42,4 +53,12 @@ def step_impl(context, minutes):
 
 @then("Поставить видео на паузу")
 def step_impl(context):
-    context.driver.find_element_by_xpath('//*[@aria-label="Пауза"]').click()
+    try:
+        context.driver.find_element_by_xpath('//*[@aria-label="Пауза"]').click()
+    except NoSuchElementException:
+        pass
+
+
+def skip_sc(context):
+    context.scenario.skip(require_not_executed=True)
+
