1e3b8342b59c610618bd04f87833224fe8f33105
Perverseq <luchniknya@gmail.com>
2018-10-26 16:18:32 +0500

    Убрал мощный цикл для поиска директории Сделал что-то для отправки сообщений, но сообщения все еще не отправляются

--- Features/environment.py
+++ Features/environment.py
@@ -3,12 +3,13 @@ from selenium import webdriver
 import os
 
 def before_all(context):
-    context.driver = webdriver.Chrome(".//chromedriver.exe")
-    dirs = os.listdir()
-    create = True
-    for index in range(len(dirs)):
-        if dirs[index] == "Screens":
-            create == False
-            return
-    if create == True:
-        os.makedirs(".\\Screens\\")
+    context.driver = webdriver.Chrome(os.path.abspath("chromedriver.exe"))
+    context.driver.maximize_window()
+    if os.path.exists("Screens"):
+        print("Папка уже создана\n")
+    else:
+        os.makedirs(os.path.abspath("Screens"))
+    #with open(os.path.abspath("Email.txt"),"r") as maildata:
+        #context.logpass = maildata.read().split()
+    #with open(os.path.abspath("Subj.txt"), "r") as subjdata:
+        #context.subj = subjdata.read().split()
